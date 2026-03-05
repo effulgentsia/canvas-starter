89707ff660aaed9e639aff19c1e5a46f60a5d5eb
Balint Kleri <b@balintbrews.com>
2026-03-05 21:02:24 +0100

    fix(eslint-config): add JSX parsing

--- eslint.config.js
+++ eslint.config.js
@@ -3,6 +3,16 @@ import storybook from "eslint-plugin-storybook";
 
 export default [
   { ignores: ["dist", "node_modules", "storybook-static"] },
+  {
+    files: ["**/*.{js,jsx}"],
+    languageOptions: {
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+        ecmaFeatures: { jsx: true },
+      },
+    },
+  },
   // ...drupalCanvasRecommended,
   ...storybook.configs["flat/recommended"],
 ];
